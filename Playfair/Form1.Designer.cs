b50f5f34c085a7081e5bda7ebeb89789b98abc65
Warship55 <lungudumitru55@gmail.com>
2023-04-27 23:11:08 +0300

    lab3

---
+++ Playfair/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Playfair
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 145);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(43, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(43, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cryptograme";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 243);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(449, 145);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(43, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 67);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(333, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 67);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(543, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Key";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(520, 49);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(393, 55);
+            this.textBox3.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(522, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 352);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Matrix of encryption";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 56);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(362, 276);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(936, 492);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Playfair";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private TextBox textBox3;
+        private GroupBox groupBox1;
+        private TextBox textBox4;
+    }
+}
---
+++ Playfair/Form1.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Playfair
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string[,] matrix = new string[5, 5];
+        string new_Alphabet, temp;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToUpper();
+
+            textBox2.Text = textBox1.Text;
+            textBox2.Text = RemoveSpaces(textBox2.Text);
+            textBox2.Text = SeparateText(textBox2.Text);
+
+
+            textBox3.Text = textBox3.Text.ToUpper();
+            textBox3.Text = RemoveSpaces(textBox3.Text);
+            new_Alphabet = CreateNewAlphabet(textBox3.Text);
+            textBox3.Text = new_Alphabet;
+
+            //afisez matricea
+            textBox4.Text = "";
+            matrix = PrintMatrix(new_Alphabet, textBox4);
+
+            //criptez
+            temp = Encrypt(textBox2.Text, matrix);
+            textBox2.Text = temp;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox2.Text.ToUpper();
+
+            textBox3.Text = textBox3.Text.ToUpper();
+            textBox3.Text = RemoveSpaces(textBox3.Text);
+            new_Alphabet = CreateNewAlphabet(textBox3.Text);
+            textBox3.Text = new_Alphabet;
+            //afisez matricea
+            textBox4.Text = "";
+            matrix = PrintMatrix(new_Alphabet, textBox4);
+
+            //decriptarea
+            temp = Decrypt(textBox2.Text, matrix);
+
+            for (int i = 0; i < temp.Length - 2; i += 2)
+            {
+                if (temp[i] == temp[i + 2])
+                {
+                    temp = temp.Remove(i + 1, 1);
+                }
+            }
+            //if last element is x,z,y remove it
+            if (temp[temp.Length - 1] == 'X' || temp[temp.Length - 1] == 'Z' || temp[temp.Length - 1] == 'Y')
+            {
+                temp = temp.Remove(temp.Length - 1);
+            }
+            textBox1.Text = temp;
+
+
+
+        }
+
+        //sterg spatii
+        public static string RemoveSpaces(string input)
+        {
+            return new string(input.ToCharArray()
+                           .Where(c => !Char.IsWhiteSpace(c))
+                                      .ToArray());
+        }
+        //inseram X intre literele egale
+        public static string SeparateText(string input)
+        {
+            //if there is the same letter in a pair, add an X beetwen them
+            for (int i = 0; i < input.Length - 1; i += 2)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    input = input.Insert(i + 1, "X");
+                }
+            }
+
+            //if the text has an odd number of letters, add an X at the end
+            if (input.Length % 2 != 0)
+            {
+                input += "X";
+            }
+            return input;
+        }
+        //stergem dublicatele
+        public static string RemoveDublicates(string input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!output.Contains(input[i]))
+                {
+                    output += input[i];
+                }
+            }
+            return output;
+        }
+        //creem nou alfabet
+        public static string CreateNewAlphabet(string key)
+        {
+            key = RemoveDublicates(key);
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+            string newAlphabet = key;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (!newAlphabet.Contains(alphabet[i]))
+                {
+                    newAlphabet += alphabet[i];
+                }
+            }
+            RemoveDublicates(newAlphabet);
+            return newAlphabet;
+        }
+        //inseram in matrice si afisam
+        public static string[,] PrintMatrix(string new_Alphabet, TextBox textBox4)
+        {
+            string[,] matrix = new string[5, 5];
+            int k = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = new_Alphabet[k].ToString();
+                    k++;
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    textBox4.Text += matrix[i, j] + " ";
+                }
+                textBox4.Text += Environment.NewLine;
+            }
+            return matrix;
+
+        }
+
+
+
+        //functia pentru playfair de criptare
+        public static string Encrypt(string input, string[,] matrix)
+        {
+            string output = "";
+            //encryption
+            for (int i = 0; i < input.Length - 1; i += 2)
+            {
+                int row1 = 0, row2 = 0, col1 = 0, col2 = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (matrix[j, k] == input[i].ToString())
+                        {
+                            row1 = j;
+                            col1 = k;
+                        }
+                        if (matrix[j, k] == input[i + 1].ToString())
+                        {
+                            row2 = j;
+                            col2 = k;
+                        }
+                    }
+                }
+                if (row1 == row2)
+                {
+                    output += matrix[row1, (col1 + 1) % 5];
+                    output += matrix[row2, (col2 + 1) % 5];
+                }
+                else if (col1 == col2)
+                {
+                    output += matrix[(row1 + 1) % 5, col1];
+                    output += matrix[(row2 + 1) % 5, col2];
+                }
+                else
+                {
+                    output += matrix[row1, col2];
+                    output += matrix[row2, col1];
+                }
+            }
+            return output;
+        }
+        //functia pentru playfair de decriptare
+        public static string Decrypt(string input, string[,] matrix)
+        {
+            string output = "";
+            //decryption
+            for (int i = 0; i < input.Length - 1; i += 2)
+            {
+                int row1 = 0, row2 = 0, col1 = 0, col2 = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int k = 0; k < 5; k++)
+                    {
+                        if (matrix[j, k] == input[i].ToString())
+                        {
+                            row1 = j;
+                            col1 = k;
+                        }
+                        if (matrix[j, k] == input[i + 1].ToString())
+                        {
+                            row2 = j;
+                            col2 = k;
+                        }
+                    }
+                }
+                if (row1 == row2)
+                {
+                    output += matrix[row1, (col1 - 1) % 5];
+                    output += matrix[row2, (col2 - 1) % 5];
+                }
+                else if (col1 == col2)
+                {
+                    output += matrix[(row1 - 1) % 5, col1];
+                    output += matrix[(row2 - 1) % 5, col2];
+                }
+                else
+                {
+                    output += matrix[row1, col2];
+                    output += matrix[row2, col1];
+                }
+            }
+            return output;
+        }
+
+    }
+
+}
+
+
